73af0cdb2e8910e3a83bbaaa2906cd60aa1f9586
David <david.betancur.ramirez.dbr@gmail.com>
2022-06-16 16:57:39 -0500

    Autentificacion de usuario

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -54,9 +54,6 @@ const usuariosPatch = (req, res = response) => {
 const usuariosDelete = async (req, res = response) => {
     const { id } = req.params;
 
-        // Fisicamente lo borramos (no recomendado)
-    //const usuario = await Usuario.findByIdAndDelete(id);
-
     const usuario = await Usuario.findByIdAndUpdate(id, { estado: false })
 
     res.json(usuario)
--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -4,7 +4,10 @@ const { check } = require("express-validator");
 const { usuariosGet, usuariosPost, usuariosPut, usuariosPatch, usuariosDelete } = require("../controllers/usuarios");
 
 const { esRoleValido, emailExiste, existeUsuarioPorId } = require("../helpers/db-validators");
-const { validarCampos } = require("../middlewares/validar-campos");
+
+const {
+    validarCampos, validarJWT, esAdminRol, tieneRole
+} = require("../middlewares");
 
 
 const router = Router();
@@ -31,6 +34,9 @@ router.put("/:id", [
 router.patch("/", usuariosPatch)
 
 router.delete("/:id", [
+    validarJWT,
+    //esAdminRole,
+    tieneRole("ADMIN_ROLE", "VENTAS_ROLE"),
     check("id", "No es un ID valido").isMongoId(),
     check("id").custom( existeUsuarioPorId ),
     validarCampos
